eacb3809e5c6579d41417203cab6bca004b66991
suguman <sugumanb@gmail.com>
2020-04-21 23:47:59 -0500

    edit scripts

--- scripts/run.py
+++ scripts/run.py
@@ -3,9 +3,11 @@ import sys
 
 benchmark = [(5,5), (5,10), (5,15), (6,6), (6,12), (6,18), (10,10), (10,15), (10,20), (10,30), (10,35)]
 
-for (i,j) in benchmark:
-    filename = "../benchmarks/"+str(i)+"_"+str(j)+".txt"
-    outputfilename = "../outputs/"+str(i)+"_"+str(j)+".txt"
-    commandline = " ".join(["../src/./game", "-f", filename, "-df", str(3), "-id" , str(1), "-syn", ">", outputfilename])
-    print(commandline)
-    os.system(commandline)
+dflist = [4]
+for df in dflist:
+    for (i,j) in benchmark:
+        filename = "../benchmarks/"+str(i)+"_"+str(j)+".txt"
+        outputfilename = "../outputs/"+str(i)+"_"+str(j)+"_"+str(df)+".txt"
+        commandline = " ".join(["../src/./game", "-f", filename, "-df", str(df), "-id" , str(1), "-syn", ">", outputfilename])
+        print(commandline)
+        os.system(commandline)
